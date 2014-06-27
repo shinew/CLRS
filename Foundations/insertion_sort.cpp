c4319270225fd5c114de4e6f4dc8d7ef4cb2f66c
shinexwang <shinewang@live.ca>
2014-06-27 18:05:47 -0400

    retabed to spaces

--- Foundations/insertion_sort.cpp
+++ Foundations/insertion_sort.cpp
@@ -6,36 +6,36 @@
 using namespace std;
 
 void insertion_sort(int nums[], int len) {
-	for(int i = 1; i < len; i++) {
-		int j = i-1;
-		int key = nums[i];
-		while(j >= 0 && key < nums[j]) {
-			nums[j+1] = nums[j];
-			j--;
-		}
-		nums[j+1] = key;
-	}
-	return;
+    for(int i = 1; i < len; i++) {
+        int j = i-1;
+        int key = nums[i];
+        while(j >= 0 && key < nums[j]) {
+            nums[j+1] = nums[j];
+            j--;
+        }
+        nums[j+1] = key;
+    }
+    return;
 }
 
 bool insertion_sort_test() {
-	int len = 6;
-	int a[] = {5, 2, 4, 6, 1, 3};
-	int b[] = {1, 2, 3, 4, 5, 6};
-	insertion_sort(a, len);
-	for(int i = 0; i < len; i++) {
-		if (a[i] != b[i]) {
-			return false;
-		}
-	}
-	return true;
+    int len = 6;
+    int a[] = {5, 2, 4, 6, 1, 3};
+    int b[] = {1, 2, 3, 4, 5, 6};
+    insertion_sort(a, len);
+    for(int i = 0; i < len; i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main() {
-	if (insertion_sort_test()) {
-		printf("Success!");
-	} else {
-		printf("Failed.");
-	}
-	return 0;
+    if (insertion_sort_test()) {
+        printf("Success!");
+    } else {
+        printf("Failed.");
+    }
+    return 0;
 }
